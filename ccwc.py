dbe9b68349109814580d519301498ed25df6a5f8
Rohan Shanbhag <rohanshanbhag2001@gmail.com>
2024-03-16 23:41:44 -0500

    Added multibyte character counting -m flag

--- ccwc.py
+++ ccwc.py
@@ -1,5 +1,14 @@
 import argparse
 
+def getChars(fileName):
+    charCount = 0
+    with open(fileName,"rb") as file:
+        for line in file:
+            charCount += len(line.decode())
+    file.close()
+
+    return charCount
+
 def getWords(fileName):
     wordCount = 0
     with open(fileName,"r") as file:
@@ -7,6 +16,7 @@ def getWords(fileName):
             line = line.split()
             for words in line:
                 wordCount += 1
+    file.close()
 
     return wordCount
 
@@ -16,16 +26,17 @@ def getLines(fileName):
     with open(fileName,"r") as file:
         for line in file:
             lineCount += 1
-    
+    file.close()
+
     return lineCount
 
 def getBytes(fileName):
     byteCount = 0
-    with open(fileName,"r") as file:
+    with open(fileName,"rb") as file:   #Need to open with rb to read as binary
         for line in file:
             byteCount += len(line)
+    file.close()
 
-    
     return byteCount
 
 def main():
@@ -33,6 +44,7 @@ def main():
     parser.add_argument('-c', dest="fileName", type=str)
     parser.add_argument('-l', dest="fileName", type=str)
     parser.add_argument('-w', dest="fileName", type=str)
+    parser.add_argument('-m', dest="fileName", type=str)
 
     args = parser.parse_args()
     fileName = args.fileName
@@ -40,6 +52,7 @@ def main():
     print(str(getBytes(fileName)) + " " + str(fileName))
     print(str(getLines(fileName)) + " " + str(fileName))
     print(str(getWords(fileName)) + " " + str(fileName))
+    print(str(getChars(fileName)) + " " + str(fileName))
 
 if __name__ == "__main__":
     main()
